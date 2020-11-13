dd2c5adb93725f9ff81de0053b867ee5599391c2
Shreejoy Dash <shreejoy@pixelexperience.org>
2020-11-13 09:26:52 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -5,9 +5,9 @@ base_url="https://github.com/ginkgo-eleven"
 
 function remove_hals() {
     echo "Removing existing hals (if present)"
-    rm -rf hardware/qcom-caf/sm8150/media
-    rm -rf hardware/qcom-caf/sm8150/display
-    rm -rf hardware/qcom-caf/sm8150/audio
+    rm -rf hardware/qcom-caf/sm6125/media
+    rm -rf hardware/qcom-caf/sm6125/display
+    rm -rf hardware/qcom-caf/sm6125/audio
 }
 
 function remove_trees() {
@@ -32,9 +32,9 @@ function clone_hals() {
     [[ "$args" =~ .*"--force".* ]] && remove_hals
     echo "======== Cloning hals ========"
     # Git clone
-    git clone $base_url/hardware_qcom_display -b $branch hardware/qcom-caf/sm8150/display
-    git clone $base_url/hardware_qcom_audio -b $branch hardware/qcom-caf/sm8150/audio
-    git clone $base_url/hardware_qcom_media -b $branch hardware/qcom-caf/sm8150/media   
+    git clone $base_url/hardware_qcom_display -b $branch hardware/qcom-caf/sm6125/display
+    git clone $base_url/hardware_qcom_audio -b $branch hardware/qcom-caf/sm6125/audio
+    git clone $base_url/hardware_qcom_media -b $branch hardware/qcom-caf/sm6125/media   
 }
 
 if [[ "$args" =~ .*"--remove-all-nexit".* ]];then
